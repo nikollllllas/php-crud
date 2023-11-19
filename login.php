8e274b12eb2a1ad5064fe3745e40f57a5aa0dd47
Vitor Medeiros <vitor.medeiros@csptech.com.br>
2023-11-19 15:40:30 -0300

    Adiciona tela de login

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = "usuario";
+    $password = "senha";
+
+    $input_username = $_POST['username'];
+    $input_password = $_POST['password'];
+
+    if ($input_username === $username && $input_password === $password) {
+        $_SESSION['logged_in'] = true;
+
+        header("Location: pagina.php");
+        exit();
+    } else {
+        $_SESSION['login_error'] = "Usuário ou senha inválidos.";
+        header("Location: cadastro.php");
+        exit();
+    }
+} else {
+    header("Location: cadastro.php");
+    exit();
+}
